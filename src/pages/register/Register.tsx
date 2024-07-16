7b56bea61133f11194f860ca085bd695d5d60604
Ekramul <mdekramulhassan168@gmail.com>
2024-07-16 13:57:25 +0600

    useform added

--- src/pages/register/Register.tsx
+++ src/pages/register/Register.tsx
@@ -1,10 +1,24 @@
+import { SubmitHandler, useForm } from "react-hook-form";
+type TRegister = {
+  username: string;
+  email: string;
+  password: string;
+  conformPassword: string;
+};
 const Register = () => {
+  const { register, handleSubmit } = useForm<TRegister>();
+  const onSubmit: SubmitHandler<TRegister> = (data) => {
+    console.log(data);
+  };
   return (
-    <div className=" h-screen w-full  flex justify-center items-center ">
-      <div className="absolute w-[200px] h-[200px] mb-96 ml-96 lg:ml-[460px] lg:mb-[460px] bg-gradient-to-r from-[#1845ad] to-[#23a2f6] rounded-full "></div>
-      <div className="absolute w-[200px] h-[200px] -mb-96 lg:-mb-[460px] mt-40 -ml-96 lg:-ml-[460px] bg-gradient-to-r from-[#ff512f] to-[#f09819] rounded-full "></div>
+    <div className=" min-h-screen py-12 w-full  flex justify-center items-center ">
+      <div className="absolute w-[200px] h-[200px] hidden md:block mb-96 ml-96 lg:ml-[460px] lg:mb-[460px] bg-gradient-to-r from-[#1845ad] to-[#23a2f6] rounded-full "></div>
+      <div className="absolute w-[200px] h-[200px] hidden md:block -mb-96 lg:-mb-[460px] mt-40 -ml-96 lg:-ml-[460px] bg-gradient-to-r from-[#ff512f] to-[#f09819] rounded-full "></div>
       <div className="flex justify-center items-center ">
-        <form className="relative w-[400px] lg:w-[500px] mt-20 h-full  rounded-2xl backdrop-filter backdrop-blur-md border-2 border-opacity-10 shadow-lg p-8 ">
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="relative w-[360px] md:w-[500px] mt-20 h-full  rounded-2xl backdrop-filter backdrop-blur-md border-2 border-opacity-10 shadow-lg p-8 "
+        >
           <h3 className="text-4xl  text-center font-extrabold">
             Register Here
           </h3>
@@ -13,6 +27,7 @@ const Register = () => {
             Username
           </label>
           <input
+            {...register("username")}
             type="text"
             placeholder="Email or Phone"
             id="username"
@@ -22,6 +37,7 @@ const Register = () => {
             Email
           </label>
           <input
+            {...register("email")}
             type="email"
             placeholder="Email or Phone"
             id="email"
@@ -32,6 +48,7 @@ const Register = () => {
             Password
           </label>
           <input
+            {...register("password")}
             type="password"
             placeholder="Password"
             id="password"
@@ -44,13 +61,17 @@ const Register = () => {
             Conform Password
           </label>
           <input
+            {...register("conformPassword")}
             type="conform-password"
             placeholder="conform password"
             id="conform-password"
             className="block w-full h-12 mt-2 text-sm font-light   bg-opacity-10 rounded-md px-3 focus:outline-none placeholder:text-gray-300"
           />
 
-          <button className="w-full mt-12 py-3 text-lg font-semibold text-white bg-black rounded-md cursor-pointer">
+          <button
+            type="submit"
+            className="w-full mt-12 py-3 text-lg font-semibold text-white bg-black rounded-md cursor-pointer"
+          >
             Register
           </button>
         </form>

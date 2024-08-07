93fe99528b33c4214d2ea05544032974eb7dadb4
Ekramul <mdekramulhassan168@gmail.com>
2024-08-07 21:25:43 +0600

    sidebar and outlet added

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,7 +1,55 @@
+import { Layout, Menu } from "antd";
+import { createElement } from "react";
+import { Outlet } from "react-router-dom";
+
+const { Header, Content, Sider } = Layout;
+
 const Dashboard = () => {
+  const items = [
+    "UserOutlined",
+    "VideoCameraOutlined",
+    "UploadOutlined",
+    "UserOutlined",
+  ].map((icon, index) => ({
+    key: String(index + 1),
+    icon: icon,
+    label: icon,
+  }));
   return (
     <div>
-      <h1>This is Admin Dashboard</h1>
+      <Layout style={{ height: "100vh" }}>
+        <Sider
+          breakpoint="lg"
+          collapsedWidth="0"
+          onBreakpoint={(broken) => {
+            console.log(broken);
+          }}
+          onCollapse={(collapsed, type) => {
+            console.log(collapsed, type);
+          }}
+        >
+          <div className="demo-logo-vertical" />
+          <Menu
+            theme="dark"
+            mode="inline"
+            defaultSelectedKeys={["4"]}
+            items={items}
+          />
+        </Sider>
+        <Layout>
+          {/* <Header style={{ padding: 0 }} /> */}
+          <Content style={{ margin: "24px 16px 0" }}>
+            <div
+              style={{
+                padding: 24,
+                minHeight: 360,
+              }}
+            >
+              <Outlet></Outlet>
+            </div>
+          </Content>
+        </Layout>
+      </Layout>
     </div>
   );
 };
